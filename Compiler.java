938575a729688b2df0c3f4ef75886c56b1d38adf
Dan Collins <microman171@gmail.com>
2014-05-19 21:52:57 +1200

    Parser works over test string.
    I'll make a shell script to pass test inputs, as that will make testing
    easier than remembering the command line script.
    
    Also, I'll rearrange the expressions.txt so all the good expressions are first
    in order of complexity followed by bad expressions in order of complexity.

---
+++ Compiler.java
@@ -0,0 +1,223 @@
+/**
+ * This class implements a regular expression compiler using the
+ * following rules:
+ *
+ * E -> T
+ * E -> TE
+ * T -> F
+ * T -> F*
+ * T -> F+
+ * T -> F?
+ * T -> F|T
+ * F -> \v
+ * F -> v
+ * F -> .
+ * F -> [L]
+ * F -> (E)
+ * L -> v
+ * L -> vL
+ *
+ * Where E is an expression, T is a term, F is a factor, L is a list
+ * and v is a vocab item (a literal).
+ *
+ * This program will accept a regex from the command line, and then
+ * output the compiled FSM to stdout.
+ *
+ * Dan Collins 1183446
+ * Severin Mahoney Marsh
+ */
+import java.util.ArrayList;
+
+public class Compiler {
+	class Node {
+		char literal;
+		int next1;
+		int next2;
+
+		public Node(char lit, int n1, int n2) {
+			literal = lit;
+			next1 = n1;
+			next2 = n2;
+		}
+
+		public String toString() {
+			return String.format("%c,%d,%d", literal, next1, next2);
+		}
+	}
+
+	private String exp;
+	private int index;
+	private ArrayList<Node> fsm;
+
+	public static char[] SPECIAL_CHARS = {'\\', '*', '+', '?', '|',
+										  '.', '(', ')', '[', ']'}; 
+
+	public Compiler(String exp) {
+		this.exp = exp;
+		fsm = new ArrayList<Node>();
+	}
+
+	public void compile() throws IllegalArgumentException {
+		index = 0;
+
+		try {
+			expression();
+		} catch (StringIndexOutOfBoundsException e) {
+			error();
+		}
+
+		if (index != exp.length())
+			error();
+	}
+
+	private void expression() throws IllegalArgumentException {
+		term();
+
+		// Test if we've reached the end of the pattern
+		if (index == exp.length())
+			return;
+
+		// Recursive if a valid number follows
+		if (isVocab(exp.charAt(index)) ||
+			exp.charAt(index) == '(' ||
+			exp.charAt(index) == '.' ||
+			exp.charAt(index) == '[')
+			expression();
+	}
+
+	private void term() throws IllegalArgumentException {
+		factor();
+
+		// Test if we've reached the end of the pattern
+		if (index == exp.length())
+			return;
+
+		// Zero or more
+		if (exp.charAt(index) == '*') {
+			System.out.println("Consuming *");
+			index++;
+		}
+
+		// One or more
+		else if (exp.charAt(index) == '+') {
+			System.out.println("Consuming +");
+			index++;
+		}
+
+		// Zero or one
+		else if (exp.charAt(index) == '?') {
+			System.out.println("Consuming ?");
+			index++;
+		}
+
+		// Disjuction
+		else if (exp.charAt(index) == '|') {
+			System.out.println("Consuming |");
+			index++;
+
+			term();
+		}
+	}
+
+	private void factor() throws IllegalArgumentException {
+		// Escaped characters
+		if (exp.charAt(index) == '\\') {
+			System.out.println("Consuming \\");
+			index++;
+			System.out.printf("Literal %c\n", exp.charAt(index));
+			index++;
+		}
+
+		// Literals
+		else if (isVocab(exp.charAt(index))) {
+			System.out.printf("Literal %c\n", exp.charAt(index));
+			index++;
+		}
+
+		// Any literal
+		else if (exp.charAt(index) == '.') {
+			System.out.println("Consuming .");
+			index++;
+		}
+
+		// List of literals
+		else if (exp.charAt(index) == '[') {
+			System.out.println("Consuming [");
+			index++;
+			if (exp.charAt(index) == ']') {
+				System.out.println("Literal ]");
+				index++;
+			}
+			list();
+			if (exp.charAt(index) == ']') {
+				System.out.println("Consuming ]");
+				index++;
+			} else
+				error();
+		}
+
+		// Nested expression
+		else if (exp.charAt(index) == '(') {
+			System.out.println("Consuming (");
+			index++;
+			expression();
+			if (exp.charAt(index) == ')') {
+				System.out.println("Consuming )");
+				index++;
+			} else
+				error();
+		}
+
+		// This isn't a factor!
+		else
+			error();
+	}
+
+	private void list() throws IllegalArgumentException {
+		if (exp.charAt(index) != ']') {
+			System.out.printf("Literal %c\n", exp.charAt(index));
+			index++;
+			list();
+		}
+	}
+
+	private boolean isVocab(char c) {
+		for (char s : SPECIAL_CHARS) {
+			if (s == c)
+				return false;
+		}
+		
+		return true;
+	}
+
+	private void error() throws IllegalArgumentException {
+		String message = String.format("%s is an invalid regex!",
+									   exp);
+		throw new IllegalArgumentException(message);
+	}
+
+	public ArrayList<Node> getFSM() {
+		return this.fsm;
+	}
+
+	public static void main(String args[]) {
+		ArrayList<Node> fsm;
+
+		if (args.length < 1) {
+			System.err.println("This program requires a regex as an argument!");
+			return;
+		}
+
+		Compiler c = new Compiler(args[0]);
+		try {
+			c.compile();
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+		}
+
+		//fsm = c.getFSM();
+		//for (Node n : fsm) {
+		//	System.out.println(n.toString());
+		//}
+	}
+}
