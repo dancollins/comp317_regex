dfb4c137fee67dcaa854cef9ee5dbe8d7a652ad9
Severin <GazerDwarf@Gmail.com>
2014-05-22 17:07:26 +1200

    Created file for Searcher

---
+++ Searcher.java
@@ -0,0 +1,13 @@
+/**
+ * Accepts arg[0] a path to a Finite State Machine
+ * Accepts arg[1] a path to a text
+ * 		java Searcher FSM text
+ * Uses the FSM to search the text for matches
+ * Outputs each matching line once even if multiple matches exist on a line
+ * Output is standard out
+ *
+ * Dan Collins 1183446
+ * Severin Mahoney-Marsh 1181754
+ */
+public class Searcher {
+}
