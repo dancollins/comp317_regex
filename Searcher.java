3810bbe1ee14bb1f4f0a09f6dfb204f270fc8ed8
Severin <GazerDwarf@Gmail.com>
2014-05-22 17:46:53 +1200

    Started on Searcher

--- Searcher.java
+++ Searcher.java
@@ -10,4 +10,15 @@
  * Severin Mahoney-Marsh 1181754
  */
 public class Searcher {
+	// Null here is the branching state
+	Character[] consumables = new Character[]{null, 'a', 'b', null, 'c', null}
+	int[] states1 = new int[]{1, 3, 5, 2, 5, -1};
+	int[] states2 = new int[]{-1, -1, -1, 4, -1, -1}
+	Dequeue<int> deque = new Dequeue<int<();
+	String str = "This yam has abbs of steel.";
+	// Null here is the scan
+	deque.push(null);
+	// Add first state to the deque
+	deque.push(states1[0]);
+
 }
