2b74b8995314a8aa41a6e513acfbb4fe3269bf6d
Severin <GazerDwarf@Gmail.com>
2014-05-24 15:53:27 +1200

    All functions can fit on my screen, it is not as spaghettiesque now. Fixes #11

--- Searcher.java
+++ Searcher.java
@@ -35,71 +35,10 @@ public class Searcher {
 		}
 		// Unwieldy try catch for in.readLine()
 		try {
-			// The loop that tries all strings goes here
 			// String str = "This yam has abbs of steel.";
 			String str = in.readLine();
 			while (str != null){
-				// Loop to search the string
-				boolean sucess = false;
-				for (int i = 0; i < str.length(); i++){
-					Dequeue<Integer> deque = new Dequeue<Integer>();
-					// Null here is the scan
-					deque.push(null);
-					// Add first state to the deque
-					deque.push(states1[0]);
-					for (int offset = 0; i + offset < str.length(); offset++){
-						// I use a string instead of a char (or Character) as I'm skittish about using
-						// String.equals(char)
-						// This could likely be improved
-						String character = str.substring(i+offset, i+offset+1);
-						while (true){
-							Integer state = deque.pop();
-							// If scan (null) move scan, increment offset and continue
-							if (state == null) {
-								// Leave the deque empty if the scan was the only item left
-								// This signals the upper loop to break
-								if (!deque.isEmpty()){
-									deque.unshift(null);
-								}
-								break;
-							// Check if sucess 
-							// Now only handles the case where the begining state is sucess
-							} else if (state.equals(consumables.length - 1)){
-								sucess = true;
-								break;
-							// If branching push states
-							} else if (consumables[state].equals("")){
-								if (states1[state].equals(consumables.length - 1) 
-										|| states2[state].equals(consumables.length - 1)){
-									sucess = true;
-									break;
-								}
-								deque.push(states1[state]);
-								deque.push(states2[state]);
-							// Other consumables go here (Like WILD)
-							// If correct character unshift state
-							} else if (consumables[state].equals(character)) {
-								if (states1[state].equals(consumables.length - 1)) {
-									sucess = true;
-									break;
-								}
-								deque.unshift(states1[state]);
-							}
-						}
-						// Victory
-						if (sucess) {
-							break;
-						// If the deque is empty this substring has failed
-						} else if (deque.isEmpty()){
-							break;
-						}
-					}
-					// Output the satisfactory line
-					if (sucess) {
-						System.out.println(str);
-						break;
-					}
-				}
+				checkLine(consumables, states1, states2, str);
 				// Get the next line
 				str = in.readLine();
 			}
@@ -146,5 +85,71 @@ public class Searcher {
 		// Admittedly returning an Object array is also a hack.
 		return new Object[]{consumables.toArray(new String[0]), 
 					states1.toArray(new Integer[0]), states2.toArray(new Integer[0])};
+	}
+
+	private static void checkLine(String[] consumables, Integer[] states1, 
+			Integer[] states2, String str){
+		// Loop to search the string
+		for (int i = 0; i < str.length(); i++){
+			Dequeue<Integer> deque = new Dequeue<Integer>();
+			// Null here is the scan
+			deque.push(null);
+			// Add first state to the deque
+			deque.push(states1[0]);
+			// Check the substring
+			boolean sucess = checkSubstring(consumables, states1, states2, str, deque, i);
+			// Output the satisfactory line
+			if (sucess) {
+				System.out.println(str);
+				break;
+			}
+		}
+	}
+
+	private static boolean checkSubstring(String[] consumables, Integer[] states1,
+			Integer[] states2, String str, Dequeue<Integer> deque, int i){
+		for (int offset = 0; i + offset < str.length(); offset++){
+			// I use a string instead of a char (or Character) as I'm skittish about using
+			// String.equals(char)
+			// This could likely be improved
+			String character = str.substring(i+offset, i+offset+1);
+			while (true){
+				Integer state = deque.pop();
+				// If scan (null) move scan, increment offset and continue
+				if (state == null) {
+					// Leave the deque empty if the scan was the only item left
+					// This signals the upper loop to break
+					if (!deque.isEmpty()){
+						deque.unshift(null);
+					}
+					break;
+				// Check if sucess 
+				// Now only handles the case where the begining state is sucess
+				} else if (state.equals(consumables.length - 1)){
+					return true;
+				// If branching push states
+				} else if (consumables[state].equals("")){
+					if (states1[state].equals(consumables.length - 1) 
+							|| states2[state].equals(consumables.length - 1)){
+						return true;
+					}
+					deque.push(states1[state]);
+					deque.push(states2[state]);
+				// Other consumables go here (Like WILD)
+				// If correct character unshift state
+				} else if (consumables[state].equals(character)) {
+					if (states1[state].equals(consumables.length - 1)) {
+						return true;
+					}
+					deque.unshift(states1[state]);
+				}
+			}
+			// If the deque is empty this substring has failed
+			if (deque.isEmpty()){
+				return false;
+			}
+		}
+		// Ran out of characters, this substring has failed
+		return false;
 	}
 }
